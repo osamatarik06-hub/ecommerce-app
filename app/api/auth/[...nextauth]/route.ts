6a7ec779e58797df82e290497bfbe56c06ac7330
Osama <osamatarik06@gmail.com>
2026-09-04 03:20:30 +0300

    Add persistent database cart functionality

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -41,7 +41,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         return {
           id: user.id,
           email: user.email,
-          name: user.fullName || user.name,
+          name: user.fullName,
         };
       }
     })
